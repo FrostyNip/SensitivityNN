090a31f5469d4134b354fbe559f5fd4e31bbc35f
FrostyNip <noahl310@tamu.edu>
2021-07-15 18:04:05 -0500

    updated some stuff

--- main.jl
+++ main.jl
@@ -12,7 +12,7 @@ function custom_train(datapoints, noise_scalar)
     X = rand(datapoints, 2)
     Y=[]
     for i in 1:datapoints
-        push!(Y, exp(X[i,1]+X[i,2] + noise_scalar * randn(1)[1]))
+        push!(Y, sin(π/2.0*(X[i,1]+X[i,2])) + noise_scalar * randn(1)[1]))
     end
 
     data=[];
@@ -21,21 +21,21 @@ function custom_train(datapoints, noise_scalar)
     end
 
     model = Chain(
-        Dense(2,10),
-        Dense(10,10,σ),
-        Dense(10,1)
+        Dense(2,16),
+        Dense(16,16,celu),
+        Dense(16,16,celu),
+        Dense(16,1)
     )
 
     loss(x, y) = Flux.mse(model(x), y)
     para = Flux.params(model)
     opt = ADAM()
 
-    epochs=1000
+    epochs=2000
     for i in 1:epochs
-        trainset = Random.shuffle(data)[1:32]
+        trainset = Random.shuffle(data)[1:10]
         Flux.train!(loss, para, trainset, opt)
     end
-
 
     return model
 end
@@ -47,39 +47,42 @@ test2 = collect(range(0,1,length=21))
 exact=[];dim1=[];dim2=[];
 for i in 1:length(test1)
     for j in 1:length(test2)
-        push!(exact, exp(test1[i] + test2[j]))
+        push!(exact, sin(π/2.0*(test1[i] + test2[j])))
         push!(dim1, test1[i])
         push!(dim2, test2[j])
     end
 end
 
-datalength = [100,1000]
-noise_std =  [0.0,0.01]
+datalength = [10,50,100,1000,2000,10000]
+noise_std =  [0.0,0.01,0.05,0.1,0.2,0.3]
+steps = 10
 err_matrix = zeros(length(datalength),length(noise_std))
 for i in 1:length(datalength)
     for j in 1:length(noise_std)
+        for step in 1:steps
+            model = custom_train(datalength[i],noise_std[j])
 
-        model = custom_train(datalength[i],noise_std[j])
+            plotlist=[]
+            N = length(dim1)
+            for itr in 1:N
+                push!(plotlist, model([dim1[itr],dim2[itr]])[1])
+            end
 
-        plotlist=[]
-        N = length(dim1)
-        for itr in 1:N
-            push!(plotlist, model([dim1[itr],dim2[itr]])[1])
+            global mse = 0.0
+            for itr in 1:N
+                ŷ = model([dim1[itr],dim2[itr]])[1]
+                global mse += 1/N * (ŷ - exact[itr]) ^ 2
+            end
+
+            if step == 1
+                datapoints = datalength[i]
+                noise_scalar = noise_std[j]
+                scatter(dim1,dim2,plotlist,title="Points = $datapoints, σ = $noise_scalar",
+                        label="Predicted")
+                scatter!(dim1,dim2,exact,label="Exact")
+                savefig("Results/plot_N_$datapoints std_$j")
+            end
         end
-
-        global mse = 0.0
-        for itr in 1:N
-            ŷ = model([dim1[itr],dim2[itr]])[1]
-            global mse += 1/N * (ŷ - exact[itr]) ^ 2
-        end
-
-        datapoints = datalength[i]
-        noise_scalar = j
-        scatter(dim1,dim2,plotlist,title="Points = $datapoints, σ = $noise_scalar",
-                label="Predicted")
-        scatter!(dim1,dim2,exact,label="Exact")
-        savefig("plot_N_$datapoints std_$noise_scalar")
-
-        global err_matrix[i,j] = mse
+        global err_matrix[i,j] += 1/steps * mse #avg of 10 runs
     end
 end
